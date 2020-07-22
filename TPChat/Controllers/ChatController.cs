c6bfc3cf65d38315b564288af04bb597e40d6b42
ABichonENI <arnaud.bichon2019@campus-eni.fr>
2020-07-22 10:39:42 +0200

    firstTPChat

---
+++ TPChat/Controllers/ChatController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TPChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Chat/Create
+        
+
+        // POST: Chat/Create
+        
+
+        // GET: Chat/Edit/5
+       
+
+        // POST: Chat/Edit/5
+        
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
